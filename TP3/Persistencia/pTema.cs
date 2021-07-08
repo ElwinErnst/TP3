7dd85bbb18072a749d1475be197c46649d1383ed
ElwinErnst <80267349+ElwinErnst@users.noreply.github.com>
2021-07-08 16:13:39 -0300

    TP2 de programacion

---
+++ TP3/Persistencia/pTema.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using TP3.Entidades;
+using TP3.Controladores;
+
+namespace TP3.Persistencia
+{
+    class pTema
+    {
+        public static Tema GetTemaById(int id)
+        {
+            Tema t = new Tema();
+            SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Tema WHERE Id=@id");
+            cmd.Parameters.Add(new SQLiteParameter("@id", id));
+            cmd.Connection = Conexion.Connection;
+            SQLiteDataReader obdr = cmd.ExecuteReader();
+            while (obdr.Read())
+            {
+                t.Id = obdr.GetInt32(0);
+                t.Nombre = obdr.GetString(1);
+            }
+            return t;
+        }
+
+        public static List<Tema> GetAll()
+        {
+            List<Tema> temas = new List<Tema>();
+
+            SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Tema");
+            cmd.Connection = Conexion.Connection;
+            SQLiteDataReader obdr = cmd.ExecuteReader();
+            while (obdr.Read())
+            {
+                Tema t = new Tema();
+                t.Id = obdr.GetInt32(0);
+                t.Nombre = obdr.GetString(1);
+                temas.Add(t);
+            }
+            return temas;
+        }
+    }
+}
---
+++ TP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using TP3.Entidades;
+using TP3.Controladores;
+
+
+
+namespace TP3
+{
+    class Program
+    {
+        //public static List<Usuario> usuarios;
+        public static int idusuario;
+        public static int puntos = 0;
+
+        static void Main(string[] args)
+        {
+            //usuarios = new List<Usuario>();
+            Conexion.OpenConexion();
+            nUsuario.Menu();
+            Conexion.CloseConexion();
+        }
+    }
+}
